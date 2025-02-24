bf622c2aaa0114757179004be2f382765828177f
Julitown46 <morenojulian4502@gmail.com>
2025-02-24 20:28:01 +0100

    Lista las bases de datos

--- src/app/components/empresa/listar-empresas/listar-empresas.component.ts
+++ src/app/components/empresa/listar-empresas/listar-empresas.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Empresa } from '../../../models/empresa';
+import { EmpresaService } from '../../../services/empresa.service';
 
 @Component({
   selector: 'app-listar-empresas',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './listar-empresas.component.html',
   styleUrl: './listar-empresas.component.css'
 })
 export class ListarEmpresasComponent {
 
+  empresas: Empresa[] = [];
+
+  constructor(private empresaService: EmpresaService){
+    this.empresaService.getEmpresas().subscribe(e => this.empresas = e);
+  }
+
+  eliminarEmpresa(id: string){
+    this.empresaService.deleteEmpresa(id).subscribe(() => this.empresaService.getEmpresas().subscribe(e => this.empresas = e));
+  }
 }
--- src/app/components/videojuego/listar-videojuegos/listar-videojuegos.component.ts
+++ src/app/components/videojuego/listar-videojuegos/listar-videojuegos.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Videojuego } from '../../../models/videojuego';
+import { VideojuegoService } from '../../../services/videojuego.service';
+import { EmpresaService } from '../../../services/empresa.service';
+import { Empresa } from '../../../models/empresa';
 
 @Component({
   selector: 'app-listar-videojuegos',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './listar-videojuegos.component.html',
   styleUrl: './listar-videojuegos.component.css'
 })
 export class ListarVideojuegosComponent {
+
+  videojuegos: Videojuego[] = [];
+  empresas: Empresa[] = [];
+
+  constructor(private videojuegoService: VideojuegoService, private empresaService: EmpresaService){
+    this.videojuegoService.getVideojuegos().subscribe(v => this.videojuegos = v);
+    this.empresaService.getEmpresas().subscribe(e => this.empresas = e);
+  }
+
+  eliminarVideojuego(id: string){
+    this.videojuegoService.deleteVideojuego(id).subscribe( () => this.videojuegoService.getVideojuegos().subscribe(v => this.videojuegos = v));
+  }
+
 
 }
